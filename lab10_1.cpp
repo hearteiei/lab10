7f90c39c1d433b962b28a41e99c755d606a276b9
hearteiei <supalit1@gmail.com>
2021-01-12 15:51:39 +0700

    lab 10.1 v.completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,50 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+cin.get();
+cin.get();
+cin.get();
+srand(time(0));
+int a=rand()%9;
+cout << "You will get ";
+    if (a==0)
+        {
+            cout<<"A";
+        }
+    else if (a==1)
+    {
+        cout<<"B+";
+    }
+    else if (a==2)
+    {
+        cout<<"B";
+    }
+    else if (a==3)
+    {
+        cout<<"C+";
+    }
+    else if (a==4)
+    {
+        cout<<"C";
+    }
+    else if (a==5)
+    {
+        cout<<"D+";
+    }else if (a==6)
+    {
+        cout<<"D";
+    }else if (a==7)
+    {
+        cout<<"F";
+    }else
+    {
+        cout<<"W";
+    }
+cout << " in this 261102.";
+
+    return 0;
+}
